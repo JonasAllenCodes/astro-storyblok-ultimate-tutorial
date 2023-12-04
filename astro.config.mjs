61095e94cec4b1832e20a71fd8d20862fad09a66
Jonas Allen <Webmaster@JonasAllen.Codes>
2023-12-04 13:31:33 -0800

    BUILD Connect Astro to Storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,26 @@
 import { defineConfig } from 'astro/config';
-
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
 import tailwind from "@astrojs/tailwind";
-
 import basicSsl from '@vitejs/plugin-basic-ssl';
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      apiOptions: {
+        region: "us",
+      },
+    }),
+    tailwind()
+  ],
   vite: {
     plugins: [basicSsl()],
     server: {
       https: true,
     },
   },
-  integrations: [tailwind()]
 });
