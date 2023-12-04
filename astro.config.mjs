a5d2a3c7c8492ae1f9d75f48a93240fe57d64be3
Jonas Allen <Webmaster@JonasAllen.Codes>
2023-12-04 02:18:50 -0800

    BUILD Add HTTPS on localhost run dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
   integrations: [tailwind()]
 });
